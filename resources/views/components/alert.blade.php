2cb87b243928afbc7b61e912e23036842844bbcc
quangba <truongquangba1302@gmail.com>
2025-04-22 13:20:48 +0700

    fix style

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,10 +1,11 @@
-@if($response = session()->get('response'))
-    <div class="alert dark alert-icon {{ $response['error'] ? 'alert-danger' : 'alert-success' }} alert-dismissible" role="alert">
+@if ($response = session()->get('response'))
+    <div class="alert dark alert-icon {{ $response['error'] ? 'alert-danger' : 'alert-success' }} alert-dismissible"
+        role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">×</span>
         </button>
         <i class="icon {{ $response['error'] ? 'md-alert-circle-o' : 'md-check' }}" aria-hidden="true"></i>
-        <span style="padding-left: 10px">
+        <span class="pl-20 pl-md-10">
             {{ $response['message'] }}
         </span>
     </div>
