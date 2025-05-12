84f0c7dd4f4b15cceaec4c19aa0336b24c960e64
xuandung1234 <dungmx@deha-soft.com>
2025-05-12 22:55:07 +0700

    filter_activityLog_list

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\Repositories\ActivityLogRepository;
+use App\Entities\ActivityLog;
+use Illuminate\Http\Request;
 
 class ActivityLogController extends Controller
 {
@@ -19,13 +21,33 @@ class ActivityLogController extends Controller
         $this->middleware('only_superadmin');
         $this->activityLogRepository = $activityLogRepository;
     }
-    public function index()
+    public function index(Request $request)
     {
         session_start();
         session_unset();
         $this->activityLogRepository->pushCriteria(app('Prettus\Repository\Criteria\RequestCriteria'));
-        $activityLogs = $this->activityLogRepository->with('user')->orderBy('created_at', 'DESC')->paginate();
+        $query = ActivityLog::query();
+
+        if ($request->filled('name')) {
+            $query->whereHas('user', function ($q) use ($request) {
+                $q->where('name', 'like', '%' . $request->name . '%');
+            });
+        }
+
+        if ($request->filled('email')) {
+            $query->whereHas('user', function ($q) use ($request) {
+                $q->where('email', 'like', '%' . $request->email . '%');
+            });
+        }
+
+        if ($request->filled('url')) {
+            $query->where('url', 'like', '%' . $request->url . '%');
+        }
+        $activityLogs = $query->with('user')->orderByDesc('created_at')->paginate()
+                            ->appends($request->query());;
+
         $rank = $activityLogs->firstItem();
+        $values = $request->only(['name', 'email', 'url']);
         if (request()->wantsJson()) {
             return response()->json([
                 'data' => $activityLogs,
@@ -35,6 +57,6 @@ class ActivityLogController extends Controller
             return view('includes.activityLogs.table', compact('activityLogs', 'rank'))->render();
         }
 
-        return view('pages.users.activityLog', compact('activityLogs', 'rank'));
+        return view('pages.users.activityLog', compact('activityLogs', 'rank', 'values'));
     }
 }
